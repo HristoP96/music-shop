e4bf71f27d338375b567ee553b9e6c39bb2d5f1b
HristoP96 <HristoPP@outlook.com>
2021-01-30 16:14:20 +0200

    misc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {testDecorators, testClassDecorator} from '../core/decorators';
+@testClassDecorator()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'music-shop';
+
+  @testDecorators()
+  public testing(): boolean  {
+    return 1 > 2;
+  }
+
+  constructor(){
+  }
+
+  ngOnInit() {
+    console.log(this)
+  }
 }
